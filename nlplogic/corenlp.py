c05ef7fee6bebfa1600c02a6d7569001df93b3d6
Ethan Enrique Verduzco Pérez <98627959+ethanverper@users.noreply.github.com>
2024-02-05 04:04:37 +0000

    feat: adding nlp functions and testing

---
+++ nlplogic/corenlp.py
@@ -0,0 +1,40 @@
+from textblob import TextBlob
+import wikipedia
+
+
+def search_wikipedia(name):
+    """Search wikipedia"""
+    print(f"Searching for name: {name}")
+    return wikipedia.summary(name, sentences=2, auto_suggest=False, redirect=True)
+
+
+def summarize_wikipedia(name):
+    """Summarize wikipedia"""
+
+    print(f"Finding wikipedia summary for name: {name}")
+    return wikipedia.summary(name)
+
+
+def get_text_blob(text):
+    """Gets text blob object and returns"""
+
+    blob = TextBlob(text)
+    return blob
+
+
+def get_tags(name):
+    """Find wikipedia name and return back tags"""
+
+    text = summarize_wikipedia(name)
+    blob = get_text_blob(text)
+    tags = blob.tags
+    return tags
+
+
+def get_phrases(name):
+    """Find wikipedia name and return back tags"""
+
+    text = summarize_wikipedia(name)
+    blob = get_text_blob(text)
+    phrases = blob.phrases
+    return phrases
---
+++ wikiphrases.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+import fire
+from nlplogic.corenlp import get_tags
+
+if __name__ == "__main__":
+    fire.Fire(get_tags)
