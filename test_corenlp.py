e4e201a54eeed493359898d0db8f1a5453e4f113
EC2 Default User <ec2-user@ip-172-31-45-64.ec2.internal>
2024-02-06 04:04:37 +0000

    feat: adding FastAPI

--- test_corenlp.py
+++ test_corenlp.py
@@ -1,7 +1,9 @@
 from nlplogic.corenlp import get_tags, get_phrases
- 
+
+
 def test_get_phrases():
-    assert '10th-most-populous country' in get_phrases("Mexico")
+    assert "10th-most-populous country" in get_phrases("Mexico")
+
 
 def test_get_tags():
-    assert 'Spanish' in get_tags("Mexico")[1]
+    assert "Spanish" in get_tags("Mexico")[1]
